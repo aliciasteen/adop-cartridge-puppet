a77004b818b17b5f74ffc3af62eeeead3227988d
alicia.steen <alicia.steen@accenture.com>
2017-05-05 14:55:06 +0100

    Fixed cartridge job syntax, triggers and clones

--- jenkins/jobs/dsl/jobs.groovy
+++ jenkins/jobs/dsl/jobs.groovy
@@ -1,16 +1,30 @@
-
 // Folders
 def workspaceFolderName = "${WORKSPACE_NAME}"
 def projectFolderName = "${PROJECT_NAME}"
 def projectScmNamespace = "${SCM_NAMESPACE}"
 
 // Variables
-def puppetControlRepoBranch = 'master'
-def puppetControlRepoUrl = 'ssh://jenkins@gerrit:29418/${GERRIT_PROJECT}'
+def puppetControlRepoBranch = 'development'
+def puppetControlRepo = 'adop-cartridge-puppet-control-repo'
 
 def buildAppJob = pipelineJob(projectFolderName + "/PuppetPipeline")
 
 buildAppJob.with {
+    environmentVariables {
+      env("puppetControlRepoBranch", "development")
+      env("projectFolderName", 'ssh://jenkins@gerrit:29418/${GERRIT_PROJECT}')
+    }
+    triggers {
+      gerrit {
+        events {
+          refUpdated()
+        }
+        project(projectScmNamespace + puppetControlRepo, "master")
+        configure { node ->
+          node / serverName('ADOP Gerrit')
+        }
+      }
+    }
     definition {
         cps {
             script("""
@@ -20,32 +34,33 @@ buildAppJob.with {
 
                   // Clone puppet control reop
                   stage('Clone')  {
-                    git branch: puppetControlRepoBranch, credentialsId: "adop-jenkins-master", url: puppetControlRepoUrl;
+                    git branch: puppetControlRepoBranch, credentialsId: "adop-jenkins-master", url: 'ssh://jenkins@gerrit:29418/${projectScmNamespace}/${puppetControlRepo}';
                   }
 
                   // Run puppet lint, puppet validate and lint yaml files
                   stage('Validate') {
                     sh '''docker run -t --rm \\\\
                      -v jenkins_slave_home:/jenkins_slave_home/ \\\\
-                     puppet:0.0.1 \\\\
-                     /jenkins_slave_home/$JOB_NAME/puppet/validate.sh /jenkins_slave_home/$JOB_NAME/'''
+                     puppet/puppet-agent-alpine:latest \\\\
+                     /jenkins_slave_home/$JOB_NAME/scripts/puppet_validate.sh /jenkins_slave_home/$JOB_NAME/'''
                   }
 
                   // Get user token
                   stage('Get token') {
-                    sh ' curl -k -X POST -H \'Content-Type: application/json\' -d \'{"login": "admin", "password": "password", "lifetime":"1d"}\' https://##ip##/rbac-api/v1/auth/token'
+                    sh ''' curl -k -X POST -H \'Content-Type: application/json\' -d \'{\"login\": \"admin\", \"password\": \"admin@1\", \"lifetime\":\"1d\"}\' https://##ip##/rbac-api/v1/auth/token'''
+                    // Update pe-access-token with token value
                   }
 
                   // Noop Deploy control repo to puppet
                   stage('Noop Deploy') {
-                    puppet.credentials 'pe-access-token' 
+                    puppet.credentials 'pe-access-token'
                     puppet.codeDeploy 'production', credentials: 'pe-access-token'
                     puppet.job 'production', concurrency: 10, noop: true, credentialsId: 'pe-access-token'
                   }
 
                     // Noop Deploy control repo to puppet
                   stage('Deploy') {
-                    puppet.credentials 'pe-access-token' 
+                    puppet.credentials 'pe-access-token'
                     puppet.codeDeploy 'production', credentials: 'pe-access-token'
                     puppet.job 'production', concurrency: 10, noop: false, credentialsId: 'pe-access-token'
                   }
@@ -53,4 +68,5 @@ buildAppJob.with {
               """.stripIndent())
         }
     }
+
 }
